3d4ed816df752833bea71dddd5e59f78da0a6036
Ján Jurč <jurcik46@gmail.com>
2018-08-18 16:37:51 +0200

    added Interface for model Options

---
+++ WinApi/Interfaces/Model/IApiOptionModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinApi.Interfaces.Model
+{
+    public interface IApiOptionModel
+    {
+        string ApiLink { get; set; }
+        string Apikey { get; set; }
+        string UserID { get; set; }
+        string ObjecID { get; set; }
+    }
+}
---
+++ WinApi/Interfaces/Model/ISignatureOptionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinApi.Interfaces.Model
+{
+    public interface ISignatureOptionModel
+    {
+        string ProgramPath { get; set; }
+        string ProcessName { get; set; }
+    }
+}
