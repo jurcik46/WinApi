52fc8d0b838749309a30cdc6dd4ef54f54f67b51
Ján Jurč <jurcik46@gmail.com>
2018-08-20 20:04:04 +0200

    added app name to notifimessage

--- WinApi/Messages/NotifiMessage.cs
+++ WinApi/Messages/NotifiMessage.cs
@@ -9,6 +9,7 @@ namespace WinApi.Messages
 {
     class NotifiMessage
     {
+        private string _appName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + " - ";
         private string _title;
         private string _msg;
         private NotificationType _iconType;
@@ -18,8 +19,6 @@ namespace WinApi.Messages
         public string Msg { get => _msg; set => _msg = value; }
         public NotificationType IconType { get => _iconType; set => _iconType = value; }
         public int ExpTime { get => _expTime; set => _expTime = value; }
-
-
-
+        public string AppName { get => _appName; set => _appName = value; }
     }
 }
