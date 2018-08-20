00bb7feee319e8aedb3c2c6edf060458e2f36ccc
Ján Jurč <jurcik46@gmail.com>
2018-08-20 20:04:15 +0200

    interface for pusherservice

---
+++ WinApi/Interfaces/Service/IPusherService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinApi.Interfaces.Service
+{
+    public interface IPusherService
+    {
+        void StartPusher();
+        void SendBozpStatus(string msg);
+    }
+}
