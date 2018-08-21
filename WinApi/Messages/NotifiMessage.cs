1fc4b8385dbb7ff292be3a0cda3dbd7834c75047
Ján Jurč <jurcik46@gmail.com>
2018-08-21 13:14:51 +0200

    changed default time for notifiicon

--- WinApi/Messages/NotifiMessage.cs
+++ WinApi/Messages/NotifiMessage.cs
@@ -13,7 +13,7 @@ namespace WinApi.Messages
         private string _title;
         private string _msg;
         private NotificationType _iconType;
-        private int _expTime = 10;
+        private int _expTime = 20;
 
         public string Title { get => _title; set => _title = value; }
         public string Msg { get => _msg; set => _msg = value; }
